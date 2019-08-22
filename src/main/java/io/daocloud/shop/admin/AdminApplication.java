08f4a986b577e036e8838bf5672194bb46b151d9
budong <tianpei_dong@yeah.net>
2019-08-22 15:34:35 +0800

    bug fix

--- src/main/java/io/daocloud/shop/admin/AdminApplication.java
+++ src/main/java/io/daocloud/shop/admin/AdminApplication.java
@@ -1,11 +1,9 @@
 package io.daocloud.shop.admin;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableApolloConfig
 public class AdminApplication {
 
     public static void main(String[] args) {
